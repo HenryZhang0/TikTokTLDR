754e8c814be8f02da48cb7dca7e642b0f3824328
Henry Zhang <zhangyiheng2003@gmail.com>
2022-03-09 21:55:13 -0500

    some template extras

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,79 @@ import React, { useState, useEffect} from 'react'
 
 function App() {
 
+// VARIABLES
   const [data, setData] = useState([{}])
-  
-useEffect(() => {
-  fetch("/members").then(
-    res => res.json()
-  ).then(
-    data => {
-      setData(data)
-      console.log(data)
-    }
-  )
-}, [])
+  const [userData, setUserData] = useState([{}])
+  const [user, setUser] = useState("")
+
+// UI variables
+  const [usernameInputField, setUsernameInputField] = useState("")
+
+
+
+// RUN ONCE
+  useEffect(() => {
+    fetch("/programmers").then( // fetch programmers data
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+    fetch("/user/henry").then( // fetch userData
+      res => res.json()
+    ).then(
+      data => {
+        setUserData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  const applyUsername = (event) => {
+    event.preventDefault()
+    //setUser(usernameInputField)
+    console.log("Name entered: ", user)
+  }
 
   return (
     <div>
-    <b>PEOPLE</b>
-    {(typeof data.people === 'undefined') ? (
-      <p>Loading...</p>
-    ) : (
-      data.people.map((people, i) => (
-        <p key={i}>{people}</p>
-      ))
-    )}      
+      
+      
+      <form>
+        <label>Enter account name:
+          <input
+            type="text" 
+            value={usernameInputField}
+            onChange={(e) => setUsernameInputField(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+
+      <div>
+        <b>USERNAME: </b>
+        <p>{user}</p>
+      </div>
+
+      <div>
+        <b>TIKTOK DATA</b>
+        <p>
+          {userData.username}
+        </p>
+      </div>
+      
+      <div>
+        <b>PROGRAMMERS (this is an example)</b>
+        {(typeof data.programmers === 'undefined') ? (
+          <p>Loading...</p>
+        ) : (
+          data.programmers.map((people, i) => (
+            <p key={i}>{people}</p>
+          ))
+        )}     
+      </div>
 
     </div>
   )
--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from scraper import *
 
 app = Flask(__name__)
 
-@app.route("/members")
+#### EXAMPLE ROUTE
+@app.route("/programmers")
 
-def members():
-    return {"people": ["henry", "ryan", "wasay", "angela"]}
+def programmers():
+    return {"programmers": ["henry", "ryan", "wasay", "angela"]}
+####
+
+@app.route('/user/<id>') # /user route
+def user(id):
+    data = scrape(id) # calls scraper function with id parameter
+    return data
 
 
 if __name__ == "__main__": # entry point
